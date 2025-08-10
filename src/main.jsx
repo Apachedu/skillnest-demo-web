06c57857a7900c7cdaf9cd3f750496e511f31402
Apachedu <eduskillnest@gmail.com>
2025-08-10 13:56:41 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import DemoLanding from "./components/DemoLanding.jsx";
+import DemoShowcase from "./components/DemoShowcase.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <div className="sn-watermark">
+      <div className="sn-content">
+        <Routes>
+          <Route path="/" element={<Navigate to="/demo" replace />} />
+          <Route path="/demo" element={<DemoLanding />} />
+          <Route path="/demo/:id" element={<DemoShowcase />} />
+        </Routes>
+      </div>
+    </div>
+  </BrowserRouter>
+);
